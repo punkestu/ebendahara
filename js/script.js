d86ab9f4f3a1c14a9e5a76be7c84fe71da08e4ae
punkestu <pangestubima89@gmail.com>
2022-06-08 14:53:28 +0700

    adding dashboard

--- js/script.js
+++ js/script.js
@@ -5,12 +5,24 @@ function show() {
   fog.classList.toggle("fhide");
 }
 
-function togglePw(){
+function togglePw() {
   const pw = document.querySelector("#pw");
   const cb = document.querySelector("#showPw");
-  if(cb.checked){
+  if (cb.checked) {
     pw.type = "text";
-  }else{
+  } else {
     pw.type = "password";
   }
+}
+
+var ri = 0;
+
+function banner(n) {
+  const images = document.querySelectorAll(".i");
+  const steps = document.querySelectorAll(".step");
+  images[n].classList.toggle("bhide");
+  images[ri].classList.toggle("bhide");
+  steps[n].classList.toggle("active");
+  steps[ri].classList.toggle("active");
+  ri = n;
 }
